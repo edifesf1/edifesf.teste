53e11d34494b32cb75702c1c21754570101d1034
Edifesf <edifesf@gmail.com>
2025-04-07 15:04:43 -0400

    g1

--- abre_whatsapp.py
+++ abre_whatsapp.py
@@ -8,15 +8,9 @@ from selenium.webdriver.common.by import By
 import time
 
 def abre_whatsapp(linkp):
-    try:
-        varservice = Service(ChromeDriverManager().install())
-        navegador = webdriver.Chrome(service=varservice)
-        navegador.get("https://www.google.com/")
-        exit()
-        navegador.get("https://web.whatsapp.com")
-        while len(navegador.find_elements(By.XPATH, '//*[@id="side"]/div[2]')) < 1:
-            pass
-            time.sleep(5)
-        continua_sair.continua_sair(navegador, linkp)
-    except:
-        return("erro")
+    varservice = Service(ChromeDriverManager().install())
+    navegador = webdriver.Chrome(service=varservice)
+    navegador.get("https://www.google.com/")
+    exit()
+    navegador.get("https://web.whatsapp.com")
+    time.sleep(6)
