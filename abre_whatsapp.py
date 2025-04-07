7b4e7f97e184a5d2b9918839e3e2f1036e8eb181
Edifesf <edifesf@gmail.com>
2025-04-07 14:19:08 -0400

    chrome

--- abre_whatsapp.py
+++ abre_whatsapp.py
@@ -1,12 +1,16 @@
 import continua_sair
 
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
 from selenium.webdriver.common.by import By
 import time
 
 def abre_whatsapp(linkp):
     try:
-        navegador = webdriver.Chrome()
+        varservice = Service(ChromeDriverManager().install())
+        navegador = webdriver.Chrome(service=varservice)
         navegador.get("https://web.whatsapp.com")
         while len(navegador.find_elements(By.XPATH, '//*[@id="side"]/div[2]')) < 1:
             pass
